35084ea3f944591fecad5fa64cbb0f22dd3c2ba9
Sergey Zubkov <ksevelyar@protonmail.com>
2023-12-19 16:46:55 +0300

    add dockerfile

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Market.MixProject do
   def application do
     [
       mod: {Market.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :public_key]
     ]
   end
 
